5c47d84b324a6907d16b0809a3b0240bb7a4fe7e
MINNAKOV <minakov.nikita.1997@mail.ru>
2024-07-01 13:04:15 +0300

    Add files via upload

---
+++ hpmework5.py
@@ -0,0 +1,10 @@
+#immutable_var = 4342 ,'Victory' , True
+#print(immutable_var)
+#immutable_var[1] = "Thrue" Кортеж - это неизменяемый тип данных, поэтому мы не можем заменить 2й элемент на слово Thrue
+
+mutable_list = ['congragulations', [121344,234], 13131, False]
+print(mutable_list)
+mutable_list[1][1] = 456
+print(mutable_list)
+
+
